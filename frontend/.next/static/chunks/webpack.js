0bb70fe5d774a2cfcd0e58d8adceffeec4c1f5e7
Kalakonda Vinay kumar <kalakondavinaykumar@Kalakondas-MacBook-Air.local>
2026-06-08 22:32:53 +0530

    Added the backend and admin dashboard

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f52c0193bfad1a7"; }
+/******/ 		__webpack_require__.h = function() { return "5218c9597cdad228"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
